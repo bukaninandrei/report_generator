d4e38ea1e90db27892513cbb2cc0c429e525a45a
Andrey Bukanin <bukanin@restream.rt.ru>
2019-12-10 10:07:46 +0400

    Using arrays & dictionaries

--- Task.rb
+++ Task.rb
@@ -12,6 +12,7 @@ require 'json'
 require 'pry'
 require 'date'
 require 'minitest/autorun'
+require 'benchmark'
 
 class User
   attr_reader :attributes, :sessions
@@ -52,7 +53,7 @@ def collect_stats_from_users(report, users_objects, &block)
 end
 
 def work
-  file_lines = File.read('data.txt').split("\n")
+  file_lines = File.read('data_large.txt').split("\n")
 
   users = []
   sessions = []
@@ -151,34 +152,84 @@ def work
   File.write('result.json', "#{report.to_json}\n")
 end
 
-class TestMe < Minitest::Test
-  def setup
-    File.write('result.json', '')
-    File.write('data.txt',
-'user,0,Leida,Cira,0
-session,0,0,Safari 29,87,2016-10-23
-session,0,1,Firefox 12,118,2017-02-27
-session,0,2,Internet Explorer 28,31,2017-03-28
-session,0,3,Internet Explorer 28,109,2016-09-15
-session,0,4,Safari 39,104,2017-09-27
-session,0,5,Internet Explorer 35,6,2016-09-01
-user,1,Palmer,Katrina,65
-session,1,0,Safari 17,12,2016-10-21
-session,1,1,Firefox 32,3,2016-12-20
-session,1,2,Chrome 6,59,2016-11-11
-session,1,3,Internet Explorer 10,28,2017-04-29
-session,1,4,Chrome 13,116,2016-12-28
-user,2,Gregory,Santos,86
-session,2,0,Chrome 35,6,2018-09-21
-session,2,1,Safari 49,85,2017-05-22
-session,2,2,Firefox 47,17,2018-02-02
-session,2,3,Chrome 20,84,2016-11-25
-')
+# helper.rb
+def print_memory_usage
+  memory_before = `ps -o rss= -p #{Process.pid}`.to_i
+  yield
+  memory_after = `ps -o rss= -p #{Process.pid}`.to_i
+
+  puts "Memory: #{((memory_after - memory_before) / 1024.0).round(2)} MB"
+end
+
+def print_time_spent
+  time = Benchmark.realtime do
+    yield
   end
 
-  def test_result
-    work
-    expected_result = '{"totalUsers":3,"uniqueBrowsersCount":14,"totalSessions":15,"allBrowsers":"CHROME 13,CHROME 20,CHROME 35,CHROME 6,FIREFOX 12,FIREFOX 32,FIREFOX 47,INTERNET EXPLORER 10,INTERNET EXPLORER 28,INTERNET EXPLORER 35,SAFARI 17,SAFARI 29,SAFARI 39,SAFARI 49","usersStats":{"Leida Cira":{"sessionsCount":6,"totalTime":"455 min.","longestSession":"118 min.","browsers":"FIREFOX 12, INTERNET EXPLORER 28, INTERNET EXPLORER 28, INTERNET EXPLORER 35, SAFARI 29, SAFARI 39","usedIE":true,"alwaysUsedChrome":false,"dates":["2017-09-27","2017-03-28","2017-02-27","2016-10-23","2016-09-15","2016-09-01"]},"Palmer Katrina":{"sessionsCount":5,"totalTime":"218 min.","longestSession":"116 min.","browsers":"CHROME 13, CHROME 6, FIREFOX 32, INTERNET EXPLORER 10, SAFARI 17","usedIE":true,"alwaysUsedChrome":false,"dates":["2017-04-29","2016-12-28","2016-12-20","2016-11-11","2016-10-21"]},"Gregory Santos":{"sessionsCount":4,"totalTime":"192 min.","longestSession":"85 min.","browsers":"CHROME 20, CHROME 35, FIREFOX 47, SAFARI 49","usedIE":false,"alwaysUsedChrome":false,"dates":["2018-09-21","2018-02-02","2017-05-22","2016-11-25"]}}}' + "\n"
-    assert_equal expected_result, File.read('result.json')
+  puts "Time: #{time}"
+end
+
+def measure(report)
+  puts " "
+  puts report
+  print_memory_usage do
+    print_time_spent do
+      yield
+    end
+  end
+  puts " "
+end
+
+# test
+class TestMe < Minitest::Test
+  def parse_data
+    delimiter = ','.freeze
+    literal_u = 'u'.freeze
+    literal_s = 's'.freeze
+    file = File.open('data_large.txt', 'r')
+    users = {}
+    sessions = {}
+    total_spent_idx = 2
+    max_spent_idx = 3
+
+    uniq_browsers = {}
+    uniq_dates = {}
+
+    while file
+      begin
+        line = file.readline
+        case line[0]
+        when literal_u
+          _, id, f_name, l_name, age = line.split(delimiter)
+          users[id.to_i] = ["#{f_name} #{l_name}", age.to_i, 0, 0]
+        when literal_s
+          _, u_id, _, browser, time, date = line.split(delimiter)
+          uid = u_id.to_i
+
+          time = time.to_i
+          users[uid][total_spent_idx] += time
+          users[uid][max_spent_idx] = time if time > users[uid][max_spent_idx]
+
+          browser_sym = browser.to_sym
+          uniq_browsers[browser_sym] ||= uniq_browsers.length
+
+          date_sym = date.to_sym
+          uniq_dates[date_sym] ||= uniq_dates.length
+
+          sessions[uid] ||= []
+          sessions[uid] << [uniq_browsers[browser_sym], uniq_dates[date_sym]]
+        else
+          next
+        end
+      rescue EOFError
+        break
+      end
+    end
+
+    file.close
+  end
+
+  def test_make
+    measure('Test this') { parse_data }
   end
 end
