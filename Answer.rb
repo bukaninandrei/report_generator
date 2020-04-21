9a48dbee2a0d70a899291dde41fb370c430307c2
Andrey Bukanin <bukanin@restream.rt.ru>
2020-04-21 09:37:12 +0400

    OOP style

--- Answer.rb
+++ Answer.rb
@@ -15,130 +15,152 @@ require 'minitest/autorun'
 require 'benchmark'
 
 class ReportGenerator
+  DELIMITER = ','.freeze
+  LITERAL_U = 'u'.freeze
+  LITERAL_S = 's'.freeze
+  TOTAL_SPENT_IDX = 1
+  MAX_SPENT_IDX = 2
 
-end
-
-def parse_data
-  delimiter = ','.freeze
-  literal_u = 'u'.freeze
-  literal_s = 's'.freeze
-  file = File.open('data_large.txt', 'r')
-  users = {}
-  sessions = {}
-  total_sessions_cnt = 0
-  total_spent_idx = 1
-  max_spent_idx = 2
-
-  uniq_browsers = {}
-  uniq_dates = {}
-
-  while file
-    begin
-      line = file.readline
-      case line[0]
-      when literal_u
-        _, id, f_name, l_name, _ = line.split(delimiter)
-        users[id.to_i] = ["#{f_name} #{l_name}", 0, 0]
-      when literal_s
-        _, u_id, _, browser, time, date = line.split(delimiter)
-        uid = u_id.to_i
-
-        time = time.to_i
-        users[uid][total_spent_idx] += time
-        users[uid][max_spent_idx] = time if time > users[uid][max_spent_idx]
-
-        # справочник браузеров, sym -> id
-        browser_sym = browser.to_sym
-        uniq_browsers[browser_sym] ||= uniq_browsers.length # browser sym to id map
-
-        # справочник дат, sym, -> id
-        date_sym = date.to_sym
-        uniq_dates[date_sym] ||= uniq_dates.length
-
-        # четные элементы это браузеры, нечетные - даты
-        sessions[uid] ||= []
-        sessions[uid].push(uniq_browsers[browser_sym], uniq_dates[date_sym])
-        total_sessions_cnt += 1
-      else
-        next
-      end
-    rescue EOFError
-      break
+  class << self
+    def generate(input_file = 'data_large.txt', output_file = 'answ/output.txt')
+      instance = new
+      puts Time.now
+      instance.parse_data(input_file)
+      puts Time.now
+      instance.make_report(output_file)
+      puts Time.now
+      instance.clean_up
     end
   end
 
-  file.close
+  def initialize; clean_up end
 
-  uniq_browsers = uniq_browsers.invert
-  uniq_dates = uniq_dates.invert
-
-  chrome_ids = []
-  i_explorer_ids = []
-  uniq_browsers.each do |(k, v)|
-    b = v.to_s
-    i_explorer_ids << k if b.index('Internet Explorer')
-    chrome_ids << k if b.index('Chrome')
+  def clean_up
+    @users = {}
+    @sessions = {}
+    @total_sessions_cnt = 0
+    @uniq_browsers = {}
+    @uniq_dates = {}
   end
-  chrome_ids.sort!
-  i_explorer_ids.sort!
 
-  puts 'iterating...'
-  puts Time.now
-  file_out = File.open('answ/output.txt', 'w')
-  file_out << <<~TEXT
-  {
-    "allBrowsers": "#{uniq_browsers.values.map(&:to_s).join(delimiter)}",
-    "totalSessions": #{total_sessions_cnt},
-    "totalUsers": #{users.length},
-    "uniqueBrowsersCount": #{uniq_browsers.length},
-    "usersStats": {
-  TEXT
+  def perform_user(line)
+    _, id, f_name, l_name, _ = line.split(DELIMITER)
+    @users[id.to_i] = ["#{f_name} #{l_name}", 0, 0]
+  end
 
-  is_first = true
-  true_literal = 'true'
-  false_literal = 'false'
+  def perform_session(line)
+    _, u_id, _, browser, time, date = line.split(DELIMITER)
+    uid = u_id.to_i
 
-  users.each do |(user_id, user)|
-    i = 0
-    acc = { b: [], d: [], co: true, ie: false }
-    while i < sessions[user_id].length do
-      browser_id = sessions[user_id][i]
-      date_id = sessions[user_id][i+1]
+    time = time.to_i
+    @users[uid][TOTAL_SPENT_IDX] += time
+    @users[uid][MAX_SPENT_IDX] = time if time > @users[uid][MAX_SPENT_IDX]
 
-      acc[:d] << uniq_dates[date_id][0..9]
-      acc[:b] << uniq_browsers[browser_id]
+    # справочник браузеров, sym -> id
+    browser_sym = browser.to_sym
+    @uniq_browsers[browser_sym] ||= @uniq_browsers.length # browser sym to id map
 
-      if i_explorer_ids.bsearch_index { |bs| bs == browser_id }
-        acc[:ie] = true
-        acc[:co] = false
-      else
-        acc[:co] = false unless chrome_ids.bsearch_index { |bs| bs == browser_id }
+    # справочник дат, sym, -> id
+    date_sym = date.to_sym
+    @uniq_dates[date_sym] ||= @uniq_dates.length
+
+    # четные элементы это браузеры, нечетные - даты
+    @sessions[uid] ||= []
+    @sessions[uid].push(@uniq_browsers[browser_sym], @uniq_dates[date_sym])
+    @total_sessions_cnt += 1
+  end
+
+  def parse_data(file_name)
+    file = File.open(file_name, 'r')
+
+    while file
+      begin
+        line = file.readline
+        case line[0]
+        when LITERAL_U
+          perform_user(line)
+        when LITERAL_S
+          perform_session(line)
+        else
+          next
+        end
+      rescue EOFError
+        break
       end
-
-      i += 2
     end
-    acc[:co] = false if acc[:b].empty?
 
-    joined_dates = acc[:d].empty? ? '' : ("\"#{acc[:d].join('", "')}\"")
-    file_out << delimiter unless is_first
+    file.close
+  end
+
+  def make_report(file_name)
+    @uniq_browsers = @uniq_browsers.invert
+    @uniq_dates = @uniq_dates.invert
+
+    chrome_ids = []
+    i_explorer_ids = []
+    @uniq_browsers.each do |(k, v)|
+      b = v.to_s
+      i_explorer_ids << k if b.index('Internet Explorer')
+      chrome_ids << k if b.index('Chrome')
+    end
+    chrome_ids.sort!
+    i_explorer_ids.sort!
+
+    file_out = File.open(file_name, 'w')
     file_out << <<~TEXT
-    "#{user[0]}": {
-      "alwaysUsedChrome": #{acc[:co] ? true_literal : false_literal},
-      "browsers": "#{acc[:b].join(delimiter)}",
-      "dates": [#{joined_dates}],
-      "longestSession": "#{user[max_spent_idx]}",
-      "sessionsCount": "#{sessions[user_id].length}",
-      "totalTime": "#{user[total_spent_idx]}",
-      "usedIE": #{acc[:ie] ? true_literal : false_literal}
-    }
+    {
+      "allBrowsers": "#{@uniq_browsers.values.map(&:to_s).join(DELIMITER)}",
+      "totalSessions": #{@total_sessions_cnt},
+      "totalUsers": #{@users.length},
+      "uniqueBrowsersCount": #{@uniq_browsers.length},
+      "usersStats": {
     TEXT
 
-    is_first = false
-  end
+    is_first = true
+    true_literal = 'true'
+    false_literal = 'false'
 
-  file_out << '}}'
-  file_out.close
-  puts Time.now
+    @users.each do |(user_id, user)|
+      i = 0
+      acc = { b: [], d: [], co: true, ie: false }
+      while i < @sessions[user_id].length do
+        browser_id = @sessions[user_id][i]
+        date_id = @sessions[user_id][i+1]
+
+        acc[:d] << @uniq_dates[date_id][0..9]
+        acc[:b] << @uniq_browsers[browser_id]
+
+        if i_explorer_ids.bsearch_index { |bs| bs == browser_id }
+          acc[:ie] = true
+          acc[:co] = false
+        else
+          acc[:co] = false unless chrome_ids.bsearch_index { |bs| bs == browser_id }
+        end
+
+        i += 2
+      end
+      acc[:co] = false if acc[:b].empty?
+
+      joined_dates = acc[:d].empty? ? '' : ("\"#{acc[:d].join('", "')}\"")
+      file_out << DELIMITER unless is_first
+      file_out << <<~TEXT
+      "#{user[0]}": {
+        "alwaysUsedChrome": #{acc[:co] ? true_literal : false_literal},
+        "browsers": "#{acc[:b].join(DELIMITER)}",
+        "dates": [#{joined_dates}],
+        "longestSession": "#{user[MAX_SPENT_IDX]}",
+        "sessionsCount": "#{@sessions[user_id].length}",
+        "totalTime": "#{user[TOTAL_SPENT_IDX]}",
+        "usedIE": #{acc[:ie] ? true_literal : false_literal}
+      }
+      TEXT
+
+      is_first = false
+    end
+
+    file_out << '}}'
+    file_out.close
+  end
 end
 
 # helper.rb
@@ -172,6 +194,6 @@ end
 # test
 class TestMe < Minitest::Test
   def test_make
-    measure('Test this') { parse_data }
+    measure('Test this') { ReportGenerator.generate }
   end
 end
